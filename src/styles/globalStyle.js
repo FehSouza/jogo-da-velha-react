78c855d3f5872e17a775961fc80d1ebdd8b420bb
FehSouza <engcivil.fernandasouza@gmail.com>
2022-04-10 21:37:41 -0300

    criação de pastas iniciais

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 16px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+`;
